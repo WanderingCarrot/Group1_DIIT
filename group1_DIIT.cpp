4ed9fb31bfa1070d09ce475afff9634f1b70ddd1
WanderingCarrot <wooijunanimator@gmail.com>
2025-09-04 01:29:42 +0800

    Added New files

---
+++ group1_DIIT.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+using namespace std;
+
+/* 
+Name : Chuah
+ID : 25064940
+
+Practical 1, Simple Calculator
+*/
+
+int main(){
+    //Defining Variables
+
+    int num1, num2;
+    int sum;
+
+    //Processing user inputs
+    cout << "Please enter the first number: \n";
+    cin >> num1;
+
+    cout << "Please enter the second number: \n";
+    cin >> num2 ;
+
+
+
+
+    //Running Logics
+    sum = num1 + num2;
+
+
+    //Output
+    cout << "\nSum is: " << sum << endl;
+
+
+    return 0;
+}
